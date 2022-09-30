b9a870dc5afd98a864a4f4ea498aa47d51c8796e
dsk0057 <dkafle6141@gmail.com>
2022-09-30 17:28:58 -0400

    Added comments and reformatted to look better!

--- alta3research-flask01.py
+++ alta3research-flask01.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-from datetime import date
+'''Flask Project that displays stock data'''
 
 import requests
 from flask import Flask
@@ -9,53 +8,58 @@ from flask import render_template
 from flask import request
 from flask import url_for
 
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=YAT8ZYQBD2YA3SVF'
+# The url for stock info assigned to url variable
+# symbol has been assigned 'AAPL' as the default value
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=YAT8ZYQBD2YA3SVF'
 
 app = Flask(__name__)
 
 
+# This route displays a welcome message with person's name
 @app.route("/<name>")
 def welcome(name):
     return render_template("welcome.html", name=name)
 
 
+# this route takes you to the page where you can input stock symbol for data
 @app.route("/stock")
 def stock():
     return render_template("stock.html")
 
 
+# this is where the api call happens and data is returned in json() format
 @app.route("/json")
 def get_json():
-    # return jsonify(requests.get(url).json())
     data = requests.get(url).json()
     return data
 
 
-# This is a landing point for users (a start)
-@app.route("/")  # user can land at "/"
+# This is a landing point for users
+@app.route("/")
 def start():
     return render_template("welcome.html")  # look for templates/postmaker.html
 
 
 # This is where welcome.html POSTs data to
-# A user could also browser (GET) to this location
+# Or a user can also browser (GET) to this location
 @app.route("/enter_info", methods=["POST", "GET"])
-def login():
+def enter():
     global symbol, url
     # POST would likely come from a user interacting with welcome.html
     if request.method == "POST":
         if request.form.get("sym"):  # if sym was assigned via the POST
             symbol = request.form.get("sym").upper()  # grab the value of sym from the POST
-        else:  # if a user sent a post without sym then assign value defaultuser
+        else:  # if a user sent a post without sym then assign value "AAPL"
             symbol = "AAPL"
     # GET would likely come from a user interacting with a browser
     elif request.method == "GET":
         if request.args.get("sym"):  # if sym was assigned as a parameter=value
-            symbol = request.args.get("sym").upper()  # pull sym from localhost:5060/login?sym=larry
+            symbol = request.args.get("sym").upper()
         else:  # if sym was not passed...
             symbol = "AAPL"  # ...then symbol is AAPL
+    # the url now is getting a symbol from user and replacing it in the api for call
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey=YAT8ZYQBD2YA3SVF'
-    return redirect(url_for("get_json"))  # pass back to /success with val for name
+    return redirect(url_for("get_json"))  # pass back to /json to display the data
 
 
 if __name__ == "__main__":
--- alta3research-requests02.py
+++ alta3research-requests02.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 
 from pprint import pprint
-
 import crayons
 import requests
 
+# flask api url so that we can use it to display some data
 URL = "http://127.0.0.1:2224/json"
 
+# get the data from api in json format
 resp = requests.get(URL).json()
 
+# use pprint to print data in a prettier format
 pprint(resp)
 
+# get the symbol from the returned json
 company = resp['Meta Data']['2. Symbol']
+# ask user to enter date in the provided format
 user_date = input("\nEnter the date in format (YYYY-MM-DD) you would like to check the stock performance: ")
 
+# normalize the input and display data using crayons for color
+# if the date provided by user exists in the json object, then display the data
 if user_date.strip() in resp["Time Series (Daily)"]:
     stock_performance = resp["Time Series (Daily)"][user_date]
     print(
-        f"\n{crayons.green('Here is how')} {crayons.red(company, bold=True)} {crayons.green('did on')} {crayons.magenta(user_date)}:")
+        f"\n{crayons.green('This is how')} {crayons.red(company, bold=True)} {crayons.green('did on')} {crayons.magenta(user_date)}:")
     print(f"\n{crayons.cyan('Opened at:')} {crayons.yellow(stock_performance['1. open'])}"
           f"\n{crayons.cyan('High for the day:')} {crayons.yellow(stock_performance['2. high'])}"
           f"\n{crayons.cyan('Low for the day:')} {crayons.yellow(stock_performance['3. low'])}"
           f"\n{crayons.cyan('Closed at:')} {crayons.yellow(stock_performance['4. close'])}"
           f"\n{crayons.cyan('Total volume traded:')} {crayons.yellow(stock_performance['5. volume'])}")
-
+# otherwise, let user know there is no data for the provided date
 else:
     print("There is no data for this date. The market was closed for weekend, or a holiday!")
